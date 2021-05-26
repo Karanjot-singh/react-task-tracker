8e895e2d9649bf8a1050b2224e9a8e62ffd6956b
karanjot-singh <karanjot19050@iiitd.ac.in>
2021-05-26 17:53:14 +0530

    Reminder Toggle Added

--- src/App.js
+++ src/App.js
@@ -10,27 +10,38 @@ function App() {
       id: 1,
       text: "Go Fishing",
       day: "5th July 2021",
+      reminder: true
     },
     {
       id: 2,
       text: "Go Shopping",
       day: "5th July 2021",
-
+      reminder: false
     },])
 
   //Delete Task
+
   //Usually redux or contextAPI is used to manage the state from within the component, here we create functions in app.js
   const deleteTask = (id) => {
     //To set the onCLick behaviour of FaTimes icon, Works it's way up here to call function, State gets passed down actions get passed up
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) =>
+      task.id === id ? {
+        ...task, reminder: !task.reminder
+      } : task))
+    // ... operator copies rest of the object completely aprt from the on explicitly mentioned  
+  }
+
   //Write JS directly inside JSX using {}
   return (
     <div className='container'> {/*Must contain only one root element*/}
       <Header />
-      {tasks.length>0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />)
-      : ('No Tasks Today :D')
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />)
+        : ('No Tasks Today :D')
       }
     </div>
   );
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,13 @@
 import Task from "./Task"
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete,onToggle }) => {
     return (
         <>
             {/* tasks.push() is restricted as state is immutable */}
             {/* setTasks([...tasks,{}]) */}
             {/* Note: These tasks are needed by other components hence Use Context API or Redux to  gather state  */}
             {tasks.map((task) => (
-                <Task key={task.id} task={task} onDelete={onDelete} />
+                <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
             ))}
         </>
     )
